961ac61ca1ca2d19c4b6d100dd0eb6cd34443312
iosebi2899 <45330280+iosebi2899@users.noreply.github.com>
2021-09-02 02:13:09 +0400

    hom

--- js/script.js
+++ js/script.js
@@ -7,6 +7,35 @@ $('.submit').click(function() {
         window.open('registration.html','_blank');
     }
  });
- $('.burger').click(function() {
-    $('.burger-nav').toggleClass('active-nav');
+ $('.reg-submit').click(function() {
+    
+    if(/^(?=.*\d).{11}$/.test($('#id').val())==false){
+        $('#id').addClass('error-input');
+        $('.tiny').addClass('shown');
+    }else if(!$('#olqNum').val()){
+        $('#id').removeClass('error-input');
+        $('#olqNum').addClass('error-input');
+        $('.tiny').addClass('shown');
+    }else if(!$('#name').val()){
+        $('#olqNum').removeClass('error-input');
+        $('#name').addClass('error-input');
+        $('.tiny').addClass('shown');
+    }else if(!$('#strNum').val()){
+        $('#name').removeClass('error-input');
+        $('#strNum').addClass('error-input');
+        $('.tiny').addClass('shown');
+    }else if(!$('#surname').val()){
+        $('#strNum').removeClass('error-input');
+        $('#surname').addClass('error-input');
+        $('.tiny').addClass('shown');
+    }else if($('#position').val()==""){
+        $('#surname').removeClass('error-input');
+        $('#position').addClass('error-input');
+        $('.tiny').addClass('shown');
+    }
+    else{
+        $('#id').removeClass('error-input');
+        $('#olqNum').removeClass('error-input')
+        $('.tiny').removeClass('shown');
+    }
  });
