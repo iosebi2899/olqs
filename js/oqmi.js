898a93e22983135dc0fbcb04b6c094e25bd14adf
iosebi2899 <45330280+iosebi2899@users.noreply.github.com>
2021-09-06 17:18:42 +0400

    gg

--- js/oqmi.js
+++ js/oqmi.js
@@ -23,6 +23,14 @@ window.onload = function () {
         display = document.querySelector('#time');
     startTimer(fiveMinutes, display);
 };
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode !=88&&(charCode != 120 &&(charCode < 48 || charCode > 57)))){
+        return false;
+    }
+    return true;
+}
+
 $(".form-input").keyup(function () {
   if (this.value.length == this.maxLength) {
     $(this).next('.form-input').focus();
@@ -31,3 +39,14 @@ $(".form-input").keyup(function () {
 $(document).ready(function(){
     $(".bullets").css({'height':($(".oqmi").height()+'px')});
 });
+$(window).resize(function(){
+    $(".bullets").css({'height':($(".oqmi").height()+'px')});
+});
+
+$("#minus").click(function(){
+    $('p').filter(function () {
+        return $(this).css("font-size") === "16px";
+       })
+    $(".grid-header").css("font-size",size + "px");
+    console.log(`clicked ${$('.grid-header').css('font-size')}`)
+})
