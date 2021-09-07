f07020696ca36450d82431beac298b0ce1af681d
iosebi2899 <45330280+iosebi2899@users.noreply.github.com>
2021-09-07 12:01:48 +0400

    dsoda

--- js/oqmi.js
+++ js/oqmi.js
@@ -68,7 +68,6 @@ $(document).ready(function(){
     var resize = new Array('.resizable');
     resize = resize.join(',');
 
-    setCookie("saveFontSize",`${$(resize).css('font-size')}`)
     let size = getCookie("saveFontSize");
       if(size !=""){
       $(resize).css('font-size',size)
